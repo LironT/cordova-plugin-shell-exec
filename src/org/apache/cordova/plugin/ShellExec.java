97558d304ded06c0a1f5e026c9c1bd4069110bda
liront <ltagger@gmail.com>
2016-03-28 21:08:06 +0300

    trying to use ActivityManager

--- src/org/apache/cordova/plugin/ShellExec.java
+++ src/org/apache/cordova/plugin/ShellExec.java
@@ -2,6 +2,8 @@ package org.apache.cordova.plugin;
 
 import android.content.Context;
 
+import java.util.List;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,7 +14,27 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+
+import android.os.Build;
+import android.util.Log;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.MemoryInfo;
+import android.app.ActivityManager.RunningAppProcessInfo;
+
 public class ShellExec extends CordovaPlugin {
+    private ActivityManager activityManager;
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        Activity activity = cordova.getActivity();
+        activityManager = (ActivityManager) activity.getSystemService(Activity.ACTIVITY_SERVICE);
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -30,6 +52,22 @@ public class ShellExec extends CordovaPlugin {
                     output.append(line + "\n");
                     p.waitFor();
                 }
+
+//                MemoryInfo memoryInfo = new MemoryInfo();
+//                activityManager.getMemoryInfo(memoryInfo);
+//                ActivityManager actvityManager = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+//                List<RunningAppProcessInfo> procInfos = actvityManager.getRunningAppProcesses();
+//                for(RunningAppProcessInfo runningProInfo:procInfos) {
+//                    output.append("Running Processes", "()()" + runningProInfo.processName + "\n");
+//                }
+
+
+                List<ActivityManager.RunningAppProcessInfo> runningTasks = activityManager.getRunningAppProcesses();
+                for(ActivityManager.RunningAppProcessInfo runningProInfo:runningTasks) {
+                    output.append(runningProInfo.processName + "\n");
+                }
+
+
                 exitStatus = p.exitValue();
             }
             catch (IOException e) {
