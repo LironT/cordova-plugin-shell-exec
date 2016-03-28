38312fa48c594d99722ce9c6e028d108a2b83c4e
liront <ltagger@gmail.com>
2016-03-28 19:36:47 +0300

    trying to use ActivityManager

--- src/org/apache/cordova/plugin/ShellExec.java
+++ src/org/apache/cordova/plugin/ShellExec.java
@@ -1,13 +1,16 @@
-package org.apache.cordova.ShellExec;
+package org.apache.cordova.plugin;
 
-import android.app.ActivityManager;
-import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.List;
 
 public class ShellExec extends CordovaPlugin {
 
@@ -19,21 +22,14 @@ public class ShellExec extends CordovaPlugin {
             StringBuffer output = new StringBuffer();
             int exitStatus = 100;
             try {
-//                        p = Runtime.getRuntime().exec((String) args.get(0));
-//                        BufferedReader reader = new BufferedReader(
-//                                new InputStreamReader(p.getInputStream()));
-//                        String line = "";
-//                        while ((line = reader.readLine()) != null) {
-//                                output.append(line + "\n");
-//                                p.waitFor();
-//                        }
-
-                ActivityManager actvityManager = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
-                List<RunningAppProcessInfo> procInfos = actvityManager.getRunningAppProcesses();
-                for(RunningAppProcessInfo runningProInfo:procInfos){
-                    output.append("Running Processes", "()()" + runningProInfo.processName + "\n");
+                p = Runtime.getRuntime().exec((String) args.get(0));
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(p.getInputStream()));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    output.append(line + "\n");
+                    p.waitFor();
                 }
-
                 exitStatus = p.exitValue();
             }
             catch (IOException e) {
@@ -52,3 +48,59 @@ public class ShellExec extends CordovaPlugin {
     }
 
 }
+
+
+//package org.apache.cordova.ShellExec;
+//
+//import android.app.ActivityManager;
+//import android.app.ActivityManager.RunningAppProcessInfo;
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//
+//import java.io.IOException;
+//import java.util.List;
+//
+//public class ShellExec extends CordovaPlugin {
+//
+//    @Override
+//    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+//
+//        if (action.equals("exec")) {
+//            Process p;
+//            StringBuffer output = new StringBuffer();
+//            int exitStatus = 100;
+//            try {
+////                        p = Runtime.getRuntime().exec((String) args.get(0));
+////                        BufferedReader reader = new BufferedReader(
+////                                new InputStreamReader(p.getInputStream()));
+////                        String line = "";
+////                        while ((line = reader.readLine()) != null) {
+////                                output.append(line + "\n");
+////                                p.waitFor();
+////                        }
+//
+//                ActivityManager actvityManager = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+//                List<RunningAppProcessInfo> procInfos = actvityManager.getRunningAppProcesses();
+//                for(RunningAppProcessInfo runningProInfo:procInfos){
+//                    output.append("Running Processes", "()()" + runningProInfo.processName + "\n");
+//                }
+//
+//                exitStatus = p.exitValue();
+//            }
+//            catch (IOException e) {
+//                e.printStackTrace();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//            JSONObject json = new JSONObject();
+//            json.put("exitStatus", exitStatus);
+//            json.put("output", output.toString());
+//            callbackContext.success(json);
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//}
